6734e1ab02a7d8191befd436d8c20c04d140fc08
shaonli8 <72174470+shaonli8@users.noreply.github.com>
2021-07-06 21:35:36 +0530

    Create initiateBookReturn.js

---
+++ initiateBookReturn.js
@@ -0,0 +1,24 @@
+initiateBookReturn = async ()=>{
+    //add a transaction
+    db.collection("transactions").add({
+      'studentId' : this.state.scannedStudentId,
+      'bookId' : this.state.scannedBookId,
+      'date'   : firebase.firestore.Timestamp.now().toDate(),
+      'transactionType' : "Return"
+    })
+
+    //change book status
+    db.collection("books").doc(this.state.scannedBookId).update({
+      'bookAvailability' : true
+    })
+
+    //change book status
+    db.collection("students").doc(this.state.scannedStudentId).update({
+      'numberOfBooksIssued' : firebase.firestore.FieldValue.increment(-1)
+    })
+
+    this.setState({
+      scannedStudentId : '',
+      scannedBookId : ''
+    })
+  }
